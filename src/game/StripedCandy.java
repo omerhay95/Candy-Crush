db96b32e9e1c6a002f36467a1dd821a1a5cd8865
Omer Hay <omerhay95@gmail.com>
2014-06-12 20:34:14 +0300

    added crush method in StripedCandy

--- src/game/StripedCandy.java
+++ src/game/StripedCandy.java
@@ -2,53 +2,92 @@ package game;
 
 import javax.swing.ImageIcon;
 
-
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
+ * <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
  * @generated
  */
 
-public class StripedCandy extends Candy
-{
+public class StripedCandy extends Candy {
 	private boolean isVertical;
-	
-	public StripedCandy(Candy[][] board, int row, int column, int color,boolean isVertical){
-		super(board, row, column,color);
-		
+
+	public StripedCandy(Candy[][] board, int row, int column, int color,
+			boolean isVertical) {
+		super(board, row, column, color);
+
 		String text;
-		if(isVertical)
-			text="Vertical";
+		if (isVertical)
+			text = "Vertical";
 		else {
-			text="Horizontal";
+			text = "Horizontal";
 		}
-		
-		this.setIcon(new ImageIcon("./src/pictures/pictures/"+text+COLORS.get(color)+".png"));
-		this.isVertical=isVertical;
-//		this.setText(row+", "+col);
+
+		this.setIcon(new ImageIcon("./src/pictures/pictures/" + text
+				+ COLORS.get(color) + ".png"));
+		this.isVertical = isVertical;
+		// this.setText(row+", "+col);
 	}
-	
+
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
-	
+
 	public void accept(Visitor visitor) {
-		// TODO implement me	
+		// TODO implement me
 	}
-	
+
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 * @ordered
 	 */
-	
+
 	public void visit(Candy candy) {
-		// TODO implement me	
+		// TODO implement me
 	}
 
-}
+	@Override
+	public boolean visit(RegularCandy candy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
+	@Override
+	public boolean visit(StripedCandy candy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(WrappedCandy candy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(ColorBomb candy) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void crush() {
+		super.crush();
+		int i = 0;
+		if (isVertical) {
+			while (i < board.length) {
+				board[i][col].setTransform(-1);
+				i++;
+			}
+		} else {
+			while (i < board.length) {
+				board[row][i].setTransform(-1);
+				i++;
+			}
+		}
+	}
+}
